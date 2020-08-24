de5beb851e34f1c9e6d78d8501bf955c9e6c2067
TimTran8 <tim_tran@live.ca>
2020-08-24 16:13:47 -0700

    Updates

---
+++ precache-manifest.b0783881b249e79e178736571e4789c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1cb35393e6b4badfe6ce45a7731b77",
+    "url": "/TimTran8/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/TimTran8/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3b5f1438fd6f0665692f",
+    "url": "/TimTran8/robofriends/static/css/main.278db939.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/TimTran8/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TimTran8/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b5f1438fd6f0665692f",
+    "url": "/TimTran8/robofriends/static/js/main.3bade565.chunk.js"
+  },
+  {
+    "revision": "4763215d430f3bb493ba",
+    "url": "/TimTran8/robofriends/static/js/runtime-main.b8155dc0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/TimTran8/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
